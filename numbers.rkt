77b1548331332e2d450df54adf2ed25b591bd0ad
Will Byrd <webyrd@gmail.com>
2016-01-16 21:31:42 -0700

    initial import

---
+++ numbers.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(require "mk.rkt")
+
+(provide build-num
+         unbuild-num
+         zeroo
+         poso         
+         >1o         
+         pluso
+         minuso
+         *o
+         <o
+         <=o
+         /o
+         logo
+         expo
+         appendo)
+
+(include "numbers.scm")
